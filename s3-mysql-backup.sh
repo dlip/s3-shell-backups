36b3c58cf2e46e6444ded789905e61bc9d0e4766
Josh Kraemer <joshkraemer@gmail.com>
2012-04-20 09:09:27 -0500

    Optimize DB after DB is backed up

--- s3-mysql-backup.sh
+++ s3-mysql-backup.sh
@@ -21,8 +21,8 @@ PASS='backuppass'
 # repair, optimize, and dump each database to its own sql file
 for DB in $(mysql -u$USER -p$PASS -BNe 'show databases' | grep -Ev 'mysql|information_schema|performance_schema')
 do
-mysqlcheck -h$HOST -u$USER -p$PASS --auto-repair --optimize $DB
 mysqldump -h$HOST -u$USER -p$PASS --quote-names --create-options --force $DB > $SRCDIR/$DB.sql
+mysqlcheck -h$HOST -u$USER -p$PASS --auto-repair --optimize $DB
 done
 
 # tar all the databases into $NOWDATE-backups.tar.gz
